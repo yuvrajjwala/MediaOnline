6e47d85d7bb98f55eff2dd515023f6aedbad9a7c
GauravRai2002 <88336754+GauravRai2002@users.noreply.github.com>
2022-09-17 14:20:16 +0530

    new

--- new_app/src/pages/Home.js
+++ new_app/src/pages/Home.js
@@ -9,6 +9,8 @@ import tv from '../assets/img/home/tv_1.png'
 import lock from '../assets/img/home/lock.png'
 import cash from '../assets/img/home/cash.png'
 import hamburger from '../assets/img/home/hamburger.png'
+import overflow from '../assets/img/home/071.png'
+import privateMarket from '../assets/img/home/PrivateMarketplace.png'
 function Home() {
     return (
         <>
@@ -51,34 +53,34 @@ function Home() {
             <div id='arr5'>
                 <div id='arr5-1'>
                     <div id='arr5-1-1'>
-                        <div id='arr5-1-1-1'>
-                            <img src={tv} alt=''/>
+                        <div className='div-flex' id='arr5-1-1-1'>
+                            <img className='img-block' src={tv} alt='' />
                             <span className='head-small'> Run smart tv campaigns in a privacy-safe
-                            environment.</span>
+                                environment.</span>
                         </div>
                         <div id='arr5-1-1-2' className='sub-text'>
-                        Control how much data is exposed to other parties and ensure your
-data is kept within an isolated environment.
+                            Control how much data is exposed to other parties and ensure your
+                            data is kept within an isolated environment.
                         </div>
                         {/* <div id='arr5-1-1-3'></div> */}
                     </div>
                     <div id='arr5-1-2'>
-                        <div id='arr5-1-2-1'>
-                            <img src={lock} alt=''/><span className='head-small'> Build privacy-safe audience segments</span>
+                        <div className='div-flex' id='arr5-1-2-1'>
+                            <img className='img-block' src={lock} alt='' /><span className='head-small'> Build privacy-safe audience segments</span>
                         </div>
                         <div id='arr5-1-2-2' className='sub-text'>Generate insights, build look-alike models, and support advanced
-measurement. We help build ideal customers cohorts while complying
-with privacy regulations.
-</div>
+                            measurement. We help build ideal customers cohorts while complying
+                            with privacy regulations.
+                        </div>
                         {/* <div id='arr5-1-2-3'></div> */}
                     </div>
                     <div id='arr5-1-3'>
-                        <div id='arr5-1-3-1'>
-                            <img src={cash} alt=''/><span className='head-small'> Maximise Ad Revenue and fill rate</span>
+                        <div className='div-flex' id='arr5-1-3-1'>
+                            <img className='img-block' src={cash} alt='' /><span className='head-small'> Maximise Ad Revenue and fill rate</span>
                         </div>
                         <div id='arr5-1-3-2' className='sub-text'>Maximise revenue using our hybrid auction system that allows for
-competition between SSPs, DSPs, Brands and Agencies through server
- side unified auction ensuring maximum revenue and fill.</div>
+                            competition between SSPs, DSPs, Brands and Agencies through server
+                            side unified auction ensuring maximum revenue and fill.</div>
                         {/* <div id='arr5-1-3-3'></div> */}
                     </div>
                 </div>
@@ -87,18 +89,57 @@ competition between SSPs, DSPs, Brands and Agencies through server
                         <div id='arr5-2-1-1'>
                             <span>Transfer information without the worry of data leak.</span>
                         </div>
-                        <div id='arr5-2-1-2'>
-                            <img src={hamburger} alt=''/>
+                        <div className='div-flex' id='arr5-2-1-2'>
+                            <img className='img-block' src={hamburger} alt='' />
                         </div>
                     </div>
                     <div id='arr5-2-2'>9MO ensures safety of audience data by converting personally
-identifiable information into unique identifiers.
-A marketer cannot reverse engineer our unique IDs to personally
-identifiable information (PII). This can significantly reduce the
- amount of data subject to regulations, such as (GDPR) in Europe and
-(CCPA) in the US, and increase opportunities for channels and brands
-to share data in a privacy-compliant manner.</div>
+                        identifiable information into unique identifiers.
+                        A marketer cannot reverse engineer our unique IDs to personally
+                        identifiable information (PII). This can significantly reduce the
+                        amount of data subject to regulations, such as (GDPR) in Europe and
+                        (CCPA) in the US, and increase opportunities for channels and brands
+                        to share data in a privacy-compliant manner.</div>
                 </div>
+            </div>
+
+            <div id='arr6'>
+                <div id='arr6-1'>
+                    <img src={overflow} alt='' id='arr6-img' />
+                    <div id='arr6-1-1-1'>Brands</div>
+                    <div id='arr6-1-1'>
+                        <div id='arr6-1-1-2'>
+                            <div className='heading'>High Precision Targeting</div>
+                            <div className='subHeading'>Match your own audience data across streaming channels in a privacy-safe,
+                                secure environment with unprecedented audience insights</div>
+                        </div>
+                        <div id='arr6-1-1-3'>
+                            <div className='heading'>Cross Channel Cookie-less Conversions</div>
+                            <div className='subHeading'>9MO tracks post-click conversions on an advertiser’s site by appending
+                                privacy-friendly IDs through cross channel targeting.</div>
+                        </div>
+                    </div>
+                    <div id='arr6-1-2'>
+                        <div className='heading'>A Single point of Access</div>
+                        <div className='subHeading'>Reach and measure your audience across channels with
+                            precision, scale, and transparency in a privacy-friendly way.
+                            Optimize your media investments with a view of reach, attribution and
+                            incremental impressions across TV Channels— all within a single
+                            dashboard.</div>
+                    </div>
+                    <div id='arr6-2'>
+                        <div id='arr6-2-1'>
+                            <img src={privateMarket} alt='' id='arr6-2-img' />
+                        </div>
+                        <div id='arr6-2-2'>
+                            <div id='arr6-2-2-1'>Channels</div>
+                            <div id='arr6-2-2-2'>Brands can buy PMP inventory easily and gain immediate access to 1400 pre-negotiated deals for connected TV.
+</div>
+                            <button id='arr6-2-2-3'>LEARN MORE</button>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </>
     )
