0b12e4fd428e7fb79fbff6777f0021986075b76a
Yuvraj Jwala <yuvrajjwala@gmail.com>
2022-09-20 10:54:12 +0530

    lower-contact-complete

--- new_app/src/components/Lowecontact/LowerContact.js
+++ new_app/src/components/Lowecontact/LowerContact.js
@@ -16,11 +16,11 @@ const LowerContact = () => {
            <div className='get-a-demo-form'>
               <p id='get-1'>Get a Demo</p>
               <p>Email</p>
-              <input></input>
+              <input name='email' type="email"></input>
               <p>Message</p>
-              <textarea></textarea><br></br>
+              <input id='input-2' type="text"></input><br></br>
               <button>SUBMIT</button>
-              <p className='lower-text-contact'>To learn how Clearbit handles your information, please see our privacy policy.</p>
+              <p className='lower-text-contact'>To learn how Clearbit handles your information, please see our <a href='#'>privacy policy.</a></p>
            </div>
          </div>
          </div>
