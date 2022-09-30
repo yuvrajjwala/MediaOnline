fd2ed0a595249747b86a92f82bb20c302da6de0d
Yuvraj Jwala <yuvrajjwala@gmail.com>
2022-10-01 03:05:19 +0530

    dynamic nav

--- new_app/src/components/Header.js
+++ new_app/src/components/Header.js
@@ -3,12 +3,19 @@ import { Link } from "react-router-dom";
 import './header.css';
 import logo from '../assets/img/home/Rectangle62.png';
 import back from '../assets/img/home/lines.png'
+import menu from '../assets/img/home/menu.png'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 AOS.init()
 
 function Header(props) {
+    function opencontainer(){
+        document.getElementById("mob-menu-container").style.width="100vw";
+    }
+    function closecontainer(){
+        document.getElementById("mob-menu-container").style.width="0vw";
+    }
    
 
     return (
@@ -35,11 +42,24 @@ function Header(props) {
                     </ul>
                     <div>
                     <button id="get-started"><span>{props.buttonText}</span></button>
+                    <div onClick={opencontainer} id="header-ham-menu"><img src={menu} alt="≣"></img></div>
+                    </div>
+                    <div id="mob-menu-container">
+                        <div className="mob-cont-top">
+                            <img src={logo}></img>
+                            <div onClick={closecontainer} className="X">X</div>
+                        </div>
+                        <div className="list-mob-cont">
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/channels">Channels</Link></li>
+                                <li><Link to="/brands">Brands</Link></li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
 
-                <div data-aos="fade-up"
-                    data-aos-duration="2000" className="headText">{props.text1} <span>{props.text2}</span></div>
+                <div  className="headText">{props.text1} <span>{props.text2}</span></div>
                 <div className="subText">{props.subtext1}</div>
 
                 
