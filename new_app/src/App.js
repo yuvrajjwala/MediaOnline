7e93404fcc06e282cc721cc38704e4aaab22cb43
Yuvraj Jwala <yuvrajjwala@gmail.com>
2022-10-04 17:01:24 +0530

    changes

--- new_app/src/App.js
+++ new_app/src/App.js
@@ -5,6 +5,7 @@ import Channels from './pages/Channels'
 import Brands from './pages/Brands'
 import Customer from './components/customers/customer';
 import LightHeader from './components/LighHeader/LightHeader';
+import About from './pages/About/About';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/channels" element={<Channels />}></Route>
           <Route path="/brands" element={<Brands />}></Route>
           <Route path="/customer" element={<Customer />}></Route>
+          <Route path="/about" element={<About />}></Route>
           <Route path="/light" element={<LightHeader  text1="Launch your own Video Streaming Platform" text2="" subtext1="9MO offers an end-to-end live streaming platform catering to OTT requirements of various industries via On-Demand, Live Streaming and online store." buttonText="GET STARTED" text0="INFLUENCERS," />}></Route>
         </Routes>
       </BrowserRouter>
